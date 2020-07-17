d4713d1f02a6b484badf4a97c1875fa0f3f25139
vovapasko <vovapasko1699@gmail.com>
2020-07-17 18:49:58 +0300

    Provided custom User model and routing for redis

--- server/taxi/taxi/settings.py
+++ server/taxi/taxi/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'channels',
     'rest_framework',
     'trips',
 ]
@@ -122,3 +123,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REDIS_URL = os.getenv('REDIS_URL', 'redis://localhost:6379')
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            'hosts': [REDIS_URL],
+        },
+    },
+}
