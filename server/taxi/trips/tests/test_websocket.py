09ce14e60cf683162d8422acc1f168ad30fbf017
vovapasko <vovapasko1699@gmail.com>
2020-07-19 13:31:07 +0300

    Updated Trip model and provided tests for it

--- server/taxi/trips/tests/test_websocket.py
+++ server/taxi/trips/tests/test_websocket.py
@@ -70,3 +70,36 @@ class TestWebsockets:
         )
         communicator = await auth_connect(user)
         await communicator.disconnect()
+
+    async def test_rider_can_create_trips(self, settings):
+        settings.CHANNEL_LAYERS = TEST_CHANNEL_LAYERS
+
+        user = await create_user(
+            username='rider@example.com',
+            group='rider'
+        )
+        communicator = await auth_connect(user)
+
+        # Send JSON message to server.
+        await communicator.send_json_to({
+            'type': 'create.trip',
+            'data': {
+                'pick_up_address': 'A',
+                'drop_off_address': 'B',
+                'rider': user.id,
+            }
+        })
+
+        # Receive JSON message from server.
+        response = await communicator.receive_json_from()
+        data = response.get('data')
+
+        # Confirm data.
+        assert data['id'] is not None
+        assert 'A' == data['pick_up_address']
+        assert 'B' == data['drop_off_address']
+        assert Trip.REQUESTED == data['status']
+        assert data['driver'] is None
+        assert user.username == data['rider'].get('username')
+
+        await communicator.disconnect()
